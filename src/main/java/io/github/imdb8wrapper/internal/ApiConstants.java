2c00717e70bb1657543f40c0cdaea5a4f3334dd5
gdgenchev <gdgenchev97@gmail.com>
2021-02-21 14:07:17 +0200

    Fix API Base url

--- src/main/java/io/github/imdb8wrapper/internal/ApiConstants.java
+++ src/main/java/io/github/imdb8wrapper/internal/ApiConstants.java
@@ -1,7 +1,7 @@
 package io.github.imdb8wrapper.internal;
 
 public interface ApiConstants {
-  String IMDB8_API_BASE = "https://4d99a418-fce9-485b-babc-7d9c5965266d.mock.pstmn.io";
+  String IMDB8_API_BASE = "https://imdb8.p.rapidapi.com";
   String IMDB8_API_KEY_HEADER = "x-rapidapi-key";
   String IMDB8_HOST_HEADER = "x-rapidapi-host";
   String IMDB8_HOST_VALUE = "imdb8.p.rapidapi.com";
